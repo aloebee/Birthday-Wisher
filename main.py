fe71c0426ae624317800934556aa2ca369e51fc8
aloebee <yuexu@umich.edu>
2026-03-10 16:09:48 -0400

    Update main.py for WeatherApp

--- main.py
+++ main.py
@@ -1,61 +1,40 @@
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-import pandas
-import datetime as dt
-import smtplib
+import requests
+from twilio.rest import Client
 import os
-import random
 
-today = dt.datetime.today()
-count = -1
+MY_LAT = 44.630400
+MY_LONG = -79.672107
+api_key = os.environ.get("OWM_API_KEY")
+url= "https://api.openweathermap.org/data/2.5/weather"
 
-df = pandas.read_csv("birthdays.csv", header=0)
-month = df["month"].to_list()
-day = df["day"].to_list()
-name = df["name"].to_list()
-email = df["email"].to_list()
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "units": "metric",
+    "cnt": 4,
+}
 
-for _ in month:
-    count +=1
-    print(count)
-    if _ == today.month:
-        if day[count] == today.day:
-            print(f"It's {name[count]}'s birthday!")
+response = requests.get(url, params=parameters)
+response.raise_for_status()
 
-# dict = df.to_dict("records")
-# print(dict)
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-            all_letters = os.listdir("letter_templates")
+data = response.json()
+weather_code = data["weather"][0]["id"]
+weather_desc = data["weather"][0]["description"]
+# print(weather_code)
+# print(weather_desc)
 
-            select = random.choice(all_letters)
-            print(select)
-            filename = "letter_templates/" + select
+print(weather_code)
 
-            with open(filename,"r") as f:
-                template = f.read()
-                letter = template.replace("[NAME]",name[count])
+# if 600<= weather_code <= 622:
+if weather_code >= 800:
+    account_sid = os.environ.get("ACCOUNT_SID")
+    auth_token = os.environ.get("AUTH_TOKEN")
+    client = Client(account_sid, auth_token)
 
-            with open(filename,"w") as f:
-                f.write(letter)
-
-# 4. Send the letter generated in step 3 to that person's email address.
-            sender_email = os.environ.get("SENDER_EMAIL")
-            send_pw = os.environ.get("SEND_PW")
-            
-            to_email = email[count]
-            message = f"Subject: Happy Birthday!!\n\n {letter}"
-
-            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-                # connection = smtplib.SMTP("smtp.gmail.com", port=587)
-                connection.starttls()
-                connection.login(user="xuyuesheila", password=send_pw)
-                connection.sendmail(
-                    from_addr=sender_email,
-                    to_addrs=to_email,
-                    msg=message
-                )
-
-
+    message = client.messages.create(
+        body=f"Go skiing! {weather_desc}",
+        from_='whatsapp:+14155238886',
+        to='whatsapp:+16476673284'
+    )
+    print(message.status)
